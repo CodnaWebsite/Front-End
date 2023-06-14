258d473db4764a3b9c9ffc6de7daac6e404ded78
Mobin <109798298+MrMobin1387@users.noreply.github.com>
2023-06-14 11:46:09 +0330

    main

---
+++ js/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function(){
+ 
+    Typed.new("#typed", {
+        stringsElement: document.getElementById('typed-strings'),
+        typeSpeed: 120,
+        backDelay: 1700,
+        loop: true,
+        contentType: 'html', // or text
+        // defaults to null for infinite loop
+        loopCount: null,
+        callback: function(){ foo(); },
+        resetCallback: function() { newTyped(); }
+    });
+
+    var resetElement = document.querySelector('.reset');
+    if(resetElement) {
+        resetElement.addEventListener('click', function() {
+            document.getElementById('typed')._typed.reset();
+        });
+    }
+
+});
+
+function newTyped(){ /* A new typed object */ }
+
+function foo(){ console.log("Callback"); }
+
+// Scroll Bar
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
